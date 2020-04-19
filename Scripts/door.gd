4ad12e4c984197b6c7a12f394eb6d539fe262d84
ThoCed <thonon_cedric@hotmail.com>
2020-04-19 21:38:00 +0200

    Script d'ouverture de porte via IA

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -1,50 +1,84 @@
 extends Spatial
 
+var currentTransform
+var openTransformDotPositif
+var openTransformDotNegatif
+var monster
+var player
+var opened = 0
+var elapsedTimeOpened = 0.0
+var isAcceptAction = false
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-export(NodePath) var bodyActionPath
+export var maxAngleOpen:float = 150.0
 
-var bodyAction
-var opened = false
-var tClosed
-var tOpened
-var quatClosed
-var quatOpened
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	bodyAction = get_node(bodyActionPath)
-	var r : Array
-	r.append(self)
-	r.append("Ouvrir la porte")
-	$Area.connect("body_entered",bodyAction,"_on_action",r)
-	$Area.connect("body_exited",bodyAction,"_on_close",r)
+	monster = get_node("/root/Spatial/MonsterFull")
+	player = get_node("/root/Spatial/Player")
+	currentTransform = $door.transform.basis
+	# preparation des deux transformations pour l'ouverture positif et negative de la porte
+	openTransformDotPositif = currentTransform.rotated(Vector3.UP,deg2rad(-maxAngleOpen))
+	openTransformDotNegatif = currentTransform.rotated(Vector3.UP,deg2rad(maxAngleOpen))
 	
+func _process(delta):
+	# si le dot product est positif, utilisation de la transformation positive
+	var trans
+	if opened > 0:
+		trans = $door.transform.basis.slerp(openTransformDotPositif,0.02)
+		$door.transform.basis = trans
+		elapsedTimeOpened += delta
+		if elapsedTimeOpened > 2.0:
+			opened = 0
+			elapsedTimeOpened = 0.0
 
-	#creation du Quaternion d'ouverture de porte
-	quatOpened = Quat($pivo.transform.basis.rotated(Vector3.UP,deg2rad(90.0)))
-	quatClosed = Quat($pivo.transform.basis)
-	
-	
-func _physics_process(delta):
-	if opened:
-		var q = Quat($pivo.transform.basis)
-		var r = q.slerp(quatOpened,0.01)
-		$pivo.transform.basis = Basis(r)
-	else:
-		var q = Quat($pivo.transform.basis)
-		var r = q.slerp(quatClosed,0.01)
-		$pivo.transform.basis = Basis(r)
-		
+	# si le dot product est negatif, utilisation de la transformation negative
+	if opened < 0:
+		trans = $door.transform.basis.slerp(openTransformDotNegatif,0.02)
+		$door.transform.basis = trans
+		elapsedTimeOpened += delta
+		if elapsedTimeOpened > 2.0:
+			opened = 0
+			elapsedTimeOpened = 0.0
 			
-func open():
-	opened = true
+	# sinon fermeture de la porte avec la transformation current
+	elif opened == 0:
+		trans = $door.transform.basis.slerp(currentTransform,0.02)
+		$door.transform.basis = trans
+		if $door.transform.basis.is_equal_approx(currentTransform):
+			$door/StaticBody/CollisionShape.disabled = false
 	
-func close():
-	opened = false
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Area_body_shape_entered(body_id, body, body_shape, area_shape):
+	if body == monster:
+			computeSideOfDoorOpen(monster)
+			$door/StaticBody/CollisionShape.disabled = true
+	elif body == player:
+		# c'est le player qui touche la porte, on accepte une action d'ouverture si demandée
+		acceptAction()
+		
+func computeSideOfDoorOpen(body):
+	
+	var dir = body.translation.direction_to(translation)
+	dir = dir.normalized()
+	var dot = transform.basis.x.dot(dir)
+	if dot < 0.0:
+		opened = -1
+	else:
+		opened = 1
+		
+func acceptAction():
+	isAcceptAction = true
+	
+func refuseAction():
+	isAcceptAction = false
+	
+func _input(event):
+	if isAcceptAction:
+		if event is InputEventKey:
+			if event.scancode == KEY_U:
+				computeSideOfDoorOpen(player)
+		
+	
+func _on_Area_body_shape_exited(body_id, body, body_shape, area_shape):
+	if body == player:
+		refuseAction()
+
+		

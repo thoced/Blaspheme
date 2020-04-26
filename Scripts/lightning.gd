82f813d5ea0a15788e0cd319dde74a74bdb9ce4d
ThoCed <thonon_cedric@hotmail.com>
2020-04-26 10:58:01 +0200

    update

---
+++ Scripts/lightning.gd
@@ -0,0 +1,33 @@
+extends WorldEnvironment
+
+var random
+var timeToLightning = 0.0
+var elapsedTime = 0.0
+var elapsedTimeLightning = 0.0
+var isLightning = false
+var backSkyColor:Color
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	random = RandomNumberGenerator.new()
+	random.randomize()
+	timeToLightning = random.randf_range(5.0,10.0)
+	backSkyColor = environment.background_sky.sky_top_color
+	
+func _process(delta):
+	elapsedTime += delta
+	if !isLightning and elapsedTime > timeToLightning:
+		elapsedTime = 0.0
+		random.randomize()
+		timeToLightning = random.randf_range(5.0,10.0)
+		environment.background_sky.sky_top_color = Color.white
+		isLightning = true
+	elif isLightning:
+		elapsedTimeLightning += delta
+		if elapsedTimeLightning > 0.25:
+			environment.background_sky.sky_top_color = backSkyColor
+			isLightning = false
+			elapsedTimeLightning = 0.0
+	
+	
+

64d89fa4ac6ac257654a78bbbae7355665ad1bd6
ThoCed <thonon_cedric@hotmail.com>
2020-05-03 21:06:22 +0200

    utilisation du Singleton AutoLoad PlayerVariables

--- Gui/stamina.gd
+++ Gui/stamina.gd
@@ -1,13 +1,8 @@
 extends ColorRect
 
-var player:PlayerSpecific
-
-func _ready():
-	player = get_node("/root/Spatial/Player")
-
 func _process(delta):
-	$".".margin_right =  lerp($".".margin_right,player.stamina,0.05)
-	if player.stamina < 25.0:
+	$".".margin_right =  lerp($".".margin_right,PlayerVariables.stamina,0.05)
+	if PlayerVariables.stamina < 25.0:
 		$".".color = $".".color.linear_interpolate(Color.red,0.01)
 	else:
 		$".".color = $".".color.linear_interpolate(Color.white,0.01)
